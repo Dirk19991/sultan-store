c4910cac4e7f253b1c036b8d76b0c6061bfacbb9
Dmitry Khodyrev <dooommm@mail.ru>
2023-03-28 23:48:08 +0800

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/brfhdfgjmngdjdg',
+  base: '/sultan-store/',
   plugins: [react()],
 });
